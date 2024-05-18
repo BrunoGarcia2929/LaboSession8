01704ebab2ba642f706884b8da119228707d5a8c
BrunoGarcia2929 <166246146+BrunoGarcia2929@users.noreply.github.com>
2024-05-18 13:59:36 -0500

    Add files via upload

---
+++ src/session/Entidades/SmartPhone.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package session.Entidades;
+
+import session.interfacesz.Camara;
+import session.interfacesz.Navegable;
+import session.interfacesz.Reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartPhone implements Camara,Navegable,Reproducible
+{
+
+    @Override
+    public void tomarFoto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void reproducir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+            
+}
---
+++ src/session/interfacesz/Navegable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package session.interfacesz;
+
+/**
+ *
+ * @author Estudiante
+ */
+public interface Navegable {
+     public void navegar();
+
+}
